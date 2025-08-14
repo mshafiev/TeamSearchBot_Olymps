7349f1bcb5b3b0615a246d42aea1ab77c72643ac
mikhail shafiev <m_shafiev@list.ru>
2025-08-15 00:32:31 +0300

    Revert "Refactor olympiad consumer with improved parsing, validation, and error handling (#1)"
    This reverts commit a89ef473c29c2dc0b15dd4706ff765b377b62c9a.

--- logger_config.py
+++ logger_config.py
@@ -1,29 +1,18 @@
 import logging
-import os
 
 # Настройка логгера
-logger = logging.getLogger("olymps_service")  # Название логгера
-
-# Установка уровня логирования из переменной окружения (по умолчанию INFO)
-_level = os.getenv("LOG_LEVEL", "INFO").upper()
-try:
-    logger.setLevel(getattr(logging, _level))
-except AttributeError:
-    logger.setLevel(logging.INFO)
+logger = logging.getLogger("olymps_service")   # Название логгера
+logger.setLevel(logging.DEBUG)         # Уровень логирования
 
 # Формат логов
 formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s | %(message)s')
 
-# Избежать дублирования обработчиков при повторных импортах
-if not logger.handlers:
-    # Обработчик — в консоль
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+# Обработчик — в консоль
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
-    # Обработчик — в файл
-    file_handler = logging.FileHandler("app_rmq.log")
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-logger.propagate = False
+# Обработчик — в файл
+file_handler = logging.FileHandler("app_rmq.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
